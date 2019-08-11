9ca4acf779d4cf17903620a8241891a4dfadbf3a
Esirn <31148342+Esirn@users.noreply.github.com>
2019-08-11 14:38:46 +0800

    changable

--- guess.py
+++ guess.py
@@ -1,12 +1,22 @@
 import random
 
+n = 0
+while True:
+	n = int(input("选择答案位数："))
+	if n<1 or n>10:
+		print("位数至少为1，至多为10！")
+	else:
+		break
+
 def diff(a):
 	b = []
-	for each in range(4):
+	for each in range(n):
 		b.append(a%10)
 		a = a//10
-	if b[0]==b[1] or b[0]==b[2] or b[0]==b[3] or b[1]==b[2]or b[1]==b[3]or b[2]==b[3]:
-		return False
+	for i in range(n):
+		for j in range(i+1,n):
+			if b[i]==b[j]:
+				return False
 	else:
 		return True
 
@@ -15,26 +25,39 @@ def check(a,b):
 	blue = 0
 	c = []
 	d = []
-	for i in range(4):
+	for i in range(n):
 		c.append(a%10)
 		d.append(b%10)
 		a = a//10
 		b = b//10
 		if c[i]==d[i]:
 			golden = golden+1
-	for i in range(4):
-		for j in range(4):
+	for i in range(n):
+		for j in range(n):
 			if i!=j and c[i]==d[j]:
 				blue = blue+1
 	print("Golden:{0}, Blue:{1}".format(golden, blue))
 
 while True:
-	a = random.randint(123,9877)
+	min=0
+	max=0
+	for i in range(n):
+		min = min*10+i
+		max = max*10-i+9
+	a = random.randint(min,max+1)
 	if diff(a):
 		break
 
 while True:
-	b = int(input("猜数字："))
+	c = input("猜数字：")
+	if c == "answer":
+		if a<10**(n-1):
+			print("Answer is 0" + str(a))
+		else:
+			print("Answer is " + str(a))
+		continue
+	else:
+		b = int(c)
 	if not diff(b):
 		print("RULES!")
 		continue
